2dce5e91b7815db067da932ec931c9cd5d063307
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-05-24 11:42:44 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
 microbitApp.compassButtonDown(CompassButtonOption.W, function () {
     motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 100)
+    basic.showNumber(4)
 })
 microbitApp.compassButtonDown(CompassButtonOption.N, function () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.A, function () {
+    motor.motorStopAll()
 })
 microbitApp.compassButtonDown(CompassButtonOption.S, function () {
     motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 100)
+    basic.showNumber(3)
 })
 microbitApp.compassButtonDown(CompassButtonOption.E, function () {
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
+    basic.showNumber(2)
 })
 bluetooth.startUartService()
 motor.motorStopAll()
