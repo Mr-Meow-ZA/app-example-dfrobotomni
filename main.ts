10bd4e3b7cf04d6d5e7bf70e053cf55ca48b1864
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-05-24 11:31:31 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+microbitApp.compassButtonDown(CompassButtonOption.W, function () {
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 100)
+})
+microbitApp.compassButtonDown(CompassButtonOption.N, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
+})
+microbitApp.compassButtonDown(CompassButtonOption.S, function () {
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 100)
+})
+microbitApp.compassButtonDown(CompassButtonOption.E, function () {
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
+})
+bluetooth.startUartService()
+motor.motorStopAll()
 basic.forever(function () {
 	
 })
